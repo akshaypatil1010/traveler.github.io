531eea0cd9692826124f8c9eda1dc14e15665d89
akshay <akshaypatil1239@gmail.com>
2022-11-08 12:50:12 +0530

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector('.toggle')
+const showcase = document.querySelector('.showcase')
+
+menuToggle.addEventListener("click", () => {
+    
+    menuToggle.classList.toggle('tt')
+    showcase.classList.toggle('ss')
+});
